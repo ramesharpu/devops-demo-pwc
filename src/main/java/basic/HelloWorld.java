3e98cf469a4d2a7ec8c9cf70a511b79c30a5baba
Ramesh A <ramesharpu@gmail.com>
2025-05-04 08:41:40 +0530

    added x branch code

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -3,8 +3,7 @@ package basic;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello there!!!");//from y branch(for stash demo)
-
+		System.out.println("hello there!!!");//from x branch(stash) +from y branch(for stash demo)
 	}
 
 	//from b1 branch
